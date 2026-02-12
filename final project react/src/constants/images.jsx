2ff7521e1cebed0d3d04db2f4c3cf4f55697c537
Aymen-zhr <aymenzouhari066@gmail.com>
2026-02-12 14:43:10 +0100

    structure

---
+++ final project react/src/constants/images.jsx
@@ -0,0 +1,69 @@
+import logo from "../assets/img/logo.png.webp";
+import carousel1 from "../assets/img/carousel1.jpg";
+import carousel2 from "../assets/img/carousel2.jpg";
+import carousel3 from "../assets/img/carousel3.webp";
+
+
+import card1 from "../assets/img/cards1.webp";
+import card2 from "../assets/img/cards2.webp";
+import card3 from "../assets/img/cards3.webp";
+import card4 from "../assets/img/cards4.webp";
+import card5 from "../assets/img/cards5.webp";
+import card6 from "../assets/img/cards6.webp";
+import product1 from "../assets/img/item-cart-01.jpg";
+import product2 from "../assets/img/shopy3.webp";
+import product3 from "../assets/img/shopi2.webp";
+import product4 from "../assets/img/shopy4.webp";
+import product5 from "../assets/img/shopy5.webp";
+import product6 from "../assets/img/shopy6.webp";
+import product7 from "../assets/img/shopy7.webp";
+import product8 from "../assets/img/shopy8.webp";
+
+import lookbookBanner from "../assets/img/banner-08.jpg";
+import countdownProduct from "../assets/img/shop-item.webp";
+
+import blog1 from "../assets/img/blog-01.jpg";
+import blog2 from "../assets/img/blog-02.jpg";
+import blog3 from "../assets/img/blog-03.jpg";
+
+export const images = {
+    logo,
+    carousel: {
+        carousel1,
+        carousel2,
+        carousel3,
+    },
+
+    categories: {
+        dresses: card1,
+        watches: card2,
+        footwear: card3,
+        sunglasses: card4,
+        bags: card5,
+        accessories: card6,
+    },
+
+    products: {
+        product1,
+        product2,
+        product3,
+        product4,
+        product5,
+        product6,
+        product7,
+        product8,
+    },
+
+    lookbook: {
+        banner: lookbookBanner,
+        countdownProduct,
+    },
+
+    blog: {
+        blog1,
+        blog2,
+        blog3,
+    },
+};
+
+export default images;
